b1583d36e277badcdfd3e906978b6af5190aa5b4
Kevin Hernandez :) <98708211+Kevin25DC@users.noreply.github.com>
2024-03-20 20:06:09 -0500

    add url portal helpdesk

--- server/home/urls.py
+++ server/home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('home', views.Home, name="home"),
-    path('login/', views.loginOrRegister.login_view, name='login'),
-    path('Register/', views.loginOrRegister.Register, name='Register'),
+    path('home/', views.Home, name="home"),  
+    path('login/', views.loginOrRegister.login_view, name='login'), 
+    path('Register/', views.loginOrRegister.Register, name='Register'), 
+    path('Desk',views.HelpDesk.Heldesk,name='HelpDesk'), 
 ]
